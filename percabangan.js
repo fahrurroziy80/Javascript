207b012aa78e58497918fde0d1908b67af7dd581
Fahrurroziy <148849905+fahrurroziy80@users.noreply.github.com>
2024-12-25 22:01:50 +0700

    Create percabangan.js
    membuat percabangan if/else dengan js

---
+++ percabangan.js
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    
+    <script>
+        var password = prompt("masukkan password");
+        if (password == "minggir lu miskin") {
+            document.write("<h1>pasword anda benar </h1>");
+        }else {
+            document.write("<h1>password anda salah! silakan coba lagi</h1> ");
+        }
+    </script>
+</body>
+</html>
